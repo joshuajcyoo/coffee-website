a13d29bc608d5fb41424a7c0b480dee593527dcf
Joshua Kao <98560037+joshjkao@users.noreply.github.com>
2024-09-20 12:40:49 -0700

    fetch remote data

--- src/Home.js
+++ src/Home.js
@@ -63,6 +63,36 @@ export default function Home() {
     newData.sort(sortingOption);
     setData(newData);
   }
+
+  //var csv is the CSV file with headers
+  function csvJSON(csv){
+
+    var lines=csv.split("\n");
+
+    var result = [];
+
+    // NOTE: If your columns contain commas in their values, you'll need
+    // to deal with those before doing the next step 
+    // (you might convert them to &&& or something, then covert them back later)
+    // jsfiddle showing the issue https://jsfiddle.net/
+    var headers=lines[0].split(",");
+
+    for(var i=1;i<lines.length;i++){
+
+        var obj = {};
+        var currentline=lines[i].split(",");
+
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+
+    }
+
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
+  }
   
 
   useEffect(() => {
@@ -329,6 +359,13 @@ export default function Home() {
       }
     ];
     setData(newData);
+
+    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyFAihCI4jr2oaWNMI7X4f1_x__G-y-mDqadIYIHecTwejWhRWbmKVApKMP0aqkAs4n6P_Jj4zy-HP/pub?gid=0&single=true&output=csv')
+    .then(response => response.text())
+    .then(data => {
+      var jsondata = csvJSON(data);
+        console.log(jsondata);
+    });
   }, []);
 
   return (
